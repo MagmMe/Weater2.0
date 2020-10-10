ecc35fba2f91d7eea00a86653886529309c0288d
MagmMe <marcin@magm.me>
2020-10-10 07:04:26 +0200

    Initial Commit

---
+++ WeatherApp2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp2.0
+//
+//  Created by Marcin Magiera on 10/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
