814c46ca4353e9124f2ad78d3156f35d8769040f
MagmMe <marcin@magm.me>
2020-10-10 07:34:29 +0200

    Adding instances to files

--- WeatherApp2.0/ViewController.swift
+++ WeatherApp2.0/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submit(_ sender: Any) {
+        
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=Bieruń&appid=127bd3c7027c52a48bb18c318d0e89c3")
+        print(url ?? "Udało się pobrać dane")
+        
+    }
+    
 }
 
